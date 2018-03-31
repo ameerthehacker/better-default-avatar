16e5523a45d147bb3817ff1fc687e37f40a41f6d
ameerthehacker <ameerjhanprof@gmail.com>
2018-03-31 22:10:06 +0530

    Center the short name and also select the best font size :fire:

--- src/avatar.js
+++ src/avatar.js
@@ -5,15 +5,57 @@ const Avatar = {
   generate(options) {
     // Set the options to empty object if is provided
     options = options || {};
-    const height = options.height || 40;
-    const width = options.width || 40;
+    const size = options.size || 40;
+    const name = options.name;
     const bgColor = options.bgColor || utils.getRandomColorCode();
+    let font = options.font;
 
     return new Promise((resolve, reject) => {
+      // Throw error if no name is given
+      if (!name) {
+        reject("option name is required");
+      }
+      const shortName = utils.getShortName(name);
+      let bestFontSize = 0;
       // Create a basic canvas of width * height
-      new Jimp(width, height, bgColor, (err, avatar) => {
+      new Jimp(size, size, bgColor, (err, avatar) => {
         if (!err) {
-          resolve(avatar);
+          // Decide the font to use if nothing is specified
+          if (!font) {
+            bestFontSize = utils.getBestFontSize(size);
+            // Choose the jimp provided font
+            switch (bestFontSize) {
+              case 8:
+                font = Jimp.FONT_SANS_8_WHITE;
+                break;
+              case 16:
+                font = Jimp.FONT_SANS_16_WHITE;
+                break;
+              case 32:
+                font = Jimp.FONT_SANS_32_WHITE;
+                break;
+              case 64:
+                font = Jimp.FONT_SANS_64_WHITE;
+                break;
+              case 128:
+                font = Jimp.FONT_SANS_128_WHITE;
+                break;
+            }
+          }
+          // Print the user's short name in the avatar
+          Jimp.loadFont(font, (err, font) => {
+            if (!err) {
+              avatar.print(
+                font,
+                Math.floor(size / 2 - utils.getOffset(font, shortName) / 2),
+                Math.floor(size / 2 - bestFontSize / 2),
+                shortName
+              );
+              resolve(avatar);
+            } else {
+              reject(err);
+            }
+          });
         } else {
           reject(err);
         }
--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,50 @@ const utils = {
     let colorCode = randomColor();
     // Remove the # in the start and append 0x in the begimning
     colorCode = `0x${colorCode.substr(1)}`;
+
     return parseInt(colorCode);
+  },
+  getShortName(name) {
+    const words = name.split(" ");
+    if (words.length == 0) {
+      throw new Error("name is empty");
+    } else if (words.length == 1) {
+      // Return the first character of first name in caps
+      return words[0][0].toUpperCase();
+    } else {
+      return (words[0][0] + words[words.length - 1][0]).toUpperCase();
+    }
+  },
+  // Get the font offset for the given text so that we can center it perfectly
+  getOffset(font, text) {
+    let x = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (font.chars[text[i]]) {
+        x +=
+          font.chars[text[i]].xoffset +
+          (font.kernings[text[i]] && font.kernings[text[i]][text[i + 1]]
+            ? font.kernings[text[i]][text[i + 1]]
+            : 0) +
+          (font.chars[text[i]].xadvance || 0);
+      }
+    }
+
+    return x;
+  },
+  getBestFontSize(size) {
+    const availableSizes = [8, 16, 32, 64, 128];
+    // If image size is much bigger return 128
+    let bestFontSize = availableSizes[availableSizes.length - 1];
+
+    for (let i = 0; i < availableSizes.length; i++) {
+      const thresholdSize = 2 * availableSizes[i];
+      if (size <= thresholdSize) {
+        bestFontSize = availableSizes[i];
+        break;
+      }
+    }
+
+    return bestFontSize;
   }
 };
 
